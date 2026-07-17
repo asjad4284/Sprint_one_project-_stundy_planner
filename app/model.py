db5a8c570839527a86b02baee3da01d88dde1297
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-18 03:01:41 +0500

    Adding progress table

--- app/model.py
+++ app/model.py
@@ -1,19 +1,30 @@
-from sqlalchemy import Column,Integer, Sting , Foreignkey
-from app.database import base 
+from sqlalchemy import Column,Integer, String , ForeignKey
+from app.database import Base 
 
-class user(base):
+class user(Base):
     __tablename__ ="users"
     
     id = Column (Integer , primary_key=True)
-    email = Column(Sting(100), unique = True)
-    password = Column(Sting(159))
+    email = Column(String(100), unique = True)
+    password = Column(String(159))
 
-class subject(base):
+class subject(Base):
     __tablename__ ="subjects"
     id = Column(Integer, primary_key=True)
-    name =Column(Sting(200))
-    difficulty = Column(Sting(150))
+    name =Column(String(200))
+    difficulty = Column(String(150))
     hours=Column(Integer)
-    user_id = Column(Integer , ForeignKey("users.id"))
+    user_id = Column(Integer ,ForeignKey("users.id"))
 
+class schedule(Base):
+    __tablename__ ="schedule"
+    id = Column(Integer, primary_key=True)
+    day = Column(String(200))
+    duration = Column(Integer)
+    subject_id = Column(Integer , ForeignKey("subjects.id"))
 
+class progress(Base):
+    __tablename__ ="progress"
+    id =Column(Integer ,primary_key=True)
+    status = Column(String(200))
+    schedule_id= Column(Integer, ForeignKey("schedule.id"))
