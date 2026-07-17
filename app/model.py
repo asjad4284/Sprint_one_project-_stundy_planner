f14b55a5b58add8721cc517f5dca8d28b0a64c75
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-18 02:46:31 +0500

    Adding table subjects

--- app/model.py
+++ app/model.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column,Integer, Sting , Foreginkey
+from sqlalchemy import Column,Integer, Sting , Foreignkey
 from app.database import base 
 
 class user(base):
     __tablename__ ="users"
     
-    id = Column (Integer , primarykey=True)
+    id = Column (Integer , primary_key=True)
     email = Column(Sting(100), unique = True)
     password = Column(Sting(159))
 
-    
+class subject(base):
+    __tablename__ ="subjects"
+    id = Column(Integer, primary_key=True)
+    name =Column(Sting(200))
+    difficulty = Column(Sting(150))
+    hours=Column(Integer)
+    user_id = Column(Integer , ForeignKey("users.id"))
+
 
