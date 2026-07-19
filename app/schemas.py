850adcb4a53e42c6184e357702048490a1310092
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-19 12:41:44 +0500

    Adding object mapping of progress

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel , Field, EmailStr
+from typing import Dict
 """ EmailStr checking  speical  format of email
     Field is using to validate the passwoed"""
 
 class UserCreate(BaseModel):
-    email:EmailStr
+    email: EmailStr
     password:str
 
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
 
-    class Config: #there we  its read the attributes  then convert into jason response on bases of schema
+    class Config: #there its read the attributes  then convert into jason response on bases of schema
         from_attributes = True
 
 class SubjectCreate(BaseModel):
@@ -27,5 +28,33 @@ class SubjectResponse(BaseModel):
     user_id: int
     class Config:
         from_attributes = True
+class ScheduleCreate(BaseModel):
+    day : int
+    duration :int
+    subject_id: int
+
+class ScheduleResponse(BaseModel):
+    id :int
+    day:int
+    duration:int
+    subject_id :int
+    class Config:
+        from_attribute = True
+
+class ProgressCreste(BaseModel):
+    status: str
+    schedule_id = int
+
+class ProgressResponse(BaseModel):
+    id : int
+    status : str
+    schedule_id: int
+    class Config:
+        from_attribute = True
+        
+            
+
+
+
 
 
