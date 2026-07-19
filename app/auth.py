59130379344d9a8c9d4fbeb9aa804e4554569222
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-20 04:49:16 +0500

    creating token feature

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,12 @@
 from jose import jwt ,JWTError
 from datatime import datatime ,timedelta
 from fastapi import Header , HTTPException
+SECRET_KEY ="supersecretkey"
+ALGORITHM ="HS256"
+
+def create_token(data : dict):
+    to_encode=data.copy()
+    expire =datatime.utcnow()+timedelta(hours=2)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
