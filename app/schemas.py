1896d8810412dd651e5de7354253629bf85f83a9
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-19 10:24:14 +0500

    Adding extra info

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,16 @@
 from pydantic import BaseModel , Field, EmailStr
+""" EmailStr checking  speical  format of email
+    Field is using to validate the passwoed"""
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config: #there we  its read the attributes  then convert into jason response on bases of schema
+        from_attributes = True
+
+class
