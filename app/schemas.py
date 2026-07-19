415c0d3a297befb7afa50ae79f58c64ac22bbbeb
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-19 10:44:07 +0500

    obect mapping of Schedule

--- app/schemas.py
+++ app/schemas.py
@@ -13,4 +13,19 @@ class UserResponse(BaseModel):
     class Config: #there we  its read the attributes  then convert into jason response on bases of schema
         from_attributes = True
 
-class
+class SubjectCreate(BaseModel):
+    name :str
+    difficulty :str
+    hours: int =Field(gt=0)
+    user_id : int
+
+class SubjectResponse(BaseModel):
+    id :int
+    name :str
+    difficulty : str
+    hours : int
+    user_id: int
+    class Config:
+        from_attributes = True
+
+
