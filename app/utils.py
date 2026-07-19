6364c00393ede215f31296b8fa10c6ab1361da35
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-20 04:06:00 +0500

    Adding password verfication feature

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
 import bcrypt
 def hash_password(password:str):
     return bcrypt.hashpw(password.encode(),bcrypt.gensalt()).decode()
+
+def verfiy_password(plain , hashed):
+    return bcrypt.checkpw(plain.encode(),hashed.encode())
