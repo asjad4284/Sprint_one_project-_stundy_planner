9eda263817655852ede55acd976de004f121e6b1
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-20 23:21:03 +0500

    creating new user

--- app/crud.py
+++ app/crud.py
@@ -2,4 +2,17 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from app import model
 from app.utils import hash_password, verify_password
-from 
+from app.auth import create_token
+
+Day_Map={0:"Mon",1:"Tue",2:"Wed",3:"Thu",4:"fri",5:"Sat",6:"Sun"}
+# ceating  new  user  in  database 
+def create_user(db:Session ,user):
+    existing= db.query(model.user).filter(model.user.email == user.email).first()
+
+    if existing:
+        return None
+    obj = model.user(email=user.email , password =hash_password(user.password))
+    db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
