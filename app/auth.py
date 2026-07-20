f4028bf44042aa70d9d2ab870c32bffd58b5bfb9
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-20 05:00:44 +0500

    correcting name

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,19 @@
 from jose import jwt ,JWTError
-from datatime import datatime ,timedelta
+from datetime import datetime ,timedelta
 from fastapi import Header , HTTPException
 SECRET_KEY ="supersecretkey"
 ALGORITHM ="HS256"
 
 def create_token(data : dict):
     to_encode=data.copy()
-    expire =datatime.utcnow()+timedelta(hours=2)
+    expire =datetime.utcnow()+timedelta(hours=2)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
+def verify_token(authorization:str= Header(...)):
+    token = authorization.replace("Bearer","")
+    try:
+        payload = jwt.decode(token , SECRET_KEY,algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401,detail="Invalid or expired token  ")
--- app/utils.py
+++ app/utils.py
@@ -2,5 +2,5 @@ import bcrypt
 def hash_password(password:str):
     return bcrypt.hashpw(password.encode(),bcrypt.gensalt()).decode()
 
-def verfiy_password(plain , hashed):
+def verify_password(plain , hashed):
     return bcrypt.checkpw(plain.encode(),hashed.encode())
