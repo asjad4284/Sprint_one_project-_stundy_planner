ed60a3b7ca602fb640711fb9a976def36612bc63
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-21 00:14:54 +0500

    login feature

--- app/crud.py
+++ app/crud.py
@@ -5,7 +5,7 @@ from app.utils import hash_password, verify_password
 from app.auth import create_token
 
 Day_Map={0:"Mon",1:"Tue",2:"Wed",3:"Thu",4:"fri",5:"Sat",6:"Sun"}
-# ceating  new  user  in  database 
+# creating  new  user  in  database 
 def create_user(db:Session ,user):
     existing= db.query(model.user).filter(model.user.email == user.email).first()
 
@@ -16,3 +16,14 @@ def create_user(db:Session ,user):
     db.commit()
     db.refresh(obj)
     return obj
+
+def login_user(db:Session, email ,password):
+    user= db.query(model.user).filter(model.user.email ==email).first()
+    if not  user or not verify_password(password, user.password):
+        return None
+    
+    token = create_token({"sub":user.email})
+    return{
+        "access_token" :token , "user_id":user.id
+    }
+
