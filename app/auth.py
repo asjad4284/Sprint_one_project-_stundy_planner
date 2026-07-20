afd88babbabb5350b3312c2ddf66d0e7a9ab24a7
saqlainshahx01 <shahsaqlain622@gmail.com>
2026-07-20 05:10:33 +0500

    verification feature

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,12 @@
 from jose import jwt ,JWTError
-from datetime import datetime ,timedelta
+from datetime import datetime ,timedelta , timezone
 from fastapi import Header , HTTPException
 SECRET_KEY ="supersecretkey"
 ALGORITHM ="HS256"
 
 def create_token(data : dict):
     to_encode=data.copy()
-    expire =datetime.utcnow()+timedelta(hours=2)
+    expire = datetime.now(timezone.utc) + timedelta(hours=2)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
