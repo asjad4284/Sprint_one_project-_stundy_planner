07df75241f61235a6c1c57dd81e03123760f506c
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-07-26 19:53:14 +0500

    Refactor database connection to use DATABASE_URL
    Updated database connection to use environment variable for URL.

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from  sqlalchemy.orm import  sessionmaker,declarative_base
+import os
 # At that point  you will go to MySql manully and create datbase with name of study_planner
-database_url= "mysql+pymysql://saqlain:shah001@localhost/study_planner"
-engine =create_engine(database_url)
+database_url= os.getenv("DATABASE_URL", "sqlite:///./study_planner.db")
+engine =create_engine(database_url,connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base=declarative_base()
 def get_db(): # using dependence and also for database automatically closing after using 
